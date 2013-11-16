b350494766f28fe1a559ca66c17545bb0954f860
Jean-Marc Skopek <jskopek@gmail.com>
2013-11-16 15:20:24 -0800

    Initial Commit

---
+++ YouBigSAP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YouBigSAP
+//
+//  Created by Jean-Marc Skopek on 11/16/2013.
+//  Copyright (c) 2013 Jean-Marc Skopek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YouBigSAP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YouBigSAP
+//
+//  Created by Jean-Marc Skopek on 11/16/2013.
+//  Copyright (c) 2013 Jean-Marc Skopek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
